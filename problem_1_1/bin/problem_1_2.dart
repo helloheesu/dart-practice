cf79a30d2a63500b7ba11fba28226b632d23511e
helloheesu <heesu.jung.developer@gmail.com>
2025-09-26 09:39:07 +0900

    2-2번 문제

--- problem_1_1/bin/problem_1_2.dart
+++ problem_1_1/bin/problem_1_2.dart
@@ -1,9 +1,9 @@
 const defaultCart = ["티셔츠", "바지", "모자", "티셔츠", "바지"];
 const Map<String, int> priceMap = {"티셔츠": 10000, "바지": 8000, "모자": 4000};
+const int discountThreshold = 20000;
 
 void main(List<String> arguments) {
   final List<String> cart;
-
   if (arguments.isNotEmpty && arguments.first.isNotEmpty) {
     cart = arguments.first.split(',').toList();
   } else {
@@ -14,4 +14,12 @@ void main(List<String> arguments) {
 
   // 느낌표 사이에 띄어쓰기는 문제 의도라고 가정하고 유지했습니다.
   print("장바구니에 $totalPrice원 어치를 담으셨네요 !");
+
+  final int discount;
+  if (totalPrice > discountThreshold) {
+    discount = totalPrice ~/ 10;
+    print("할인 금액 : $discount원");
+  } else {
+    discount = 0;
+  }
 }
