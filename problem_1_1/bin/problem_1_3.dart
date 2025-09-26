4505c7567aecd5069c11911e8c16efa153c5841f
helloheesu <heesu.jung.developer@gmail.com>
2025-09-26 10:38:32 +0900

    도전 문제 1-2번

--- problem_1_1/bin/problem_1_3.dart
+++ problem_1_1/bin/problem_1_3.dart
@@ -13,7 +13,25 @@ List<int> generateLottoNumbers({int count = 6, int maxNumber = 45}) {
   return sorted;
 }
 
+String getWinningGrade(int matchCount) {
+  if (matchCount >= 5) {
+    return '1등';
+  } else if (matchCount == 4) {
+    return '2등';
+  } else if (matchCount == 3) {
+    return '3등';
+  } else {
+    return '당첨 실패!';
+  }
+}
+
 void main() {
+  // final List<int> lotto = [9, 19, 29, 35, 37, 38]; // 테스트용 로또 번호
   final List<int> lotto = generateLottoNumbers();
   print('발급한 로또 번호 : $lotto');
+
+  const Set<int> winningNumbers = {9, 19, 29, 35, 37, 38};
+  final int matchCount = lotto.where((n) => winningNumbers.contains(n)).length;
+
+  print('당첨 여부 : ${getWinningGrade(matchCount)}');
 }
