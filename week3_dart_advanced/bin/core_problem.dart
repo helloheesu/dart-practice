d930896653307c3243be5f1018519d45a5025dd0
helloheesu <heesu.jung.developer@gmail.com>
2025-10-11 09:36:22 +0900

    학생 이름 선택 부분 함수로 분리

--- week3_dart_advanced/bin/core_problem.dart
+++ week3_dart_advanced/bin/core_problem.dart
@@ -1,23 +1,11 @@
-import 'dart:io';
 import 'package:week_3/services/student_loader.dart';
+import 'package:week_3/services/student_selector.dart';
 
 Future<void> main(List<String> arguments) async {
   final studentScores = await loadStudentData(
     '/Users/heesu/Documents/dart_practice/week3_dart_advanced/students.txt',
   );
 
-  while (true) {
-    stdout.write('어떤 학생의 통계를 확인하시겠습니까? ');
-    final input = stdin.readLineSync();
-    final name = (input ?? '').trim();
-
-    final existingScore = studentScores[name];
-
-    if (existingScore != null) {
-      existingScore.showInfo();
-      break;
-    } else {
-      print('잘못된 학생 이름을 입력하셨습니다. 다시 입력해주세요.');
-    }
-  }
+  final selected = readStudentNameFromStdin(studentScores);
+  selected.showInfo();
 }
