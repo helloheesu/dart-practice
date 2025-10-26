ed42c7743a4dc2d3c84ee3d4fb97062e665ab38e
helloheesu <heesu.jung.developer@gmail.com>
2025-10-27 00:37:43 +0900

    플로팅 버튼 탭시 바텀시트 노출

--- tasks/lib/widgets/add_task_button.dart
+++ tasks/lib/widgets/add_task_button.dart
@@ -3,10 +3,32 @@ import 'package:flutter/material.dart';
 class AddTaskButton extends StatelessWidget {
   const AddTaskButton({super.key});
 
+  _showAddTaskBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      useSafeArea: true,
+      isScrollControlled: true,
+      context: context,
+      builder: (context) => Padding(
+        padding: EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 12,
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: TextField(
+          controller: TextEditingController(),
+          autofocus: true,
+          style: const TextStyle(fontSize: 16),
+          decoration: const InputDecoration(hintText: '새 할 일'),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () => _showAddTaskBottomSheet(context),
       shape: const CircleBorder(),
       child: const Icon(Icons.add, color: Colors.white),
     );
