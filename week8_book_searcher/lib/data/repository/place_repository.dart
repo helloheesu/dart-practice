043722582bb271f72eadf84adea2d47e2b284db2
helloheesu <heesu.jung.developer@gmail.com>
2025-11-17 16:05:34 +0900

    ViewModel 과 테스트 작성

--- week8_book_searcher/lib/data/repository/place_repository.dart
+++ week8_book_searcher/lib/data/repository/place_repository.dart
@@ -1,24 +1,39 @@
 import 'dart:convert';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:week8_book_searcher/data/model/place.dart';
 
 class PlaceRepository {
+  PlaceRepository({http.Client? client}) : _client = client ?? http.Client();
+
+  final http.Client _client;
+
   Future<List<Place>> searchPlaces() async {
     final url = dotenv.env['API_URL'] ?? '';
     final authorization = dotenv.env['AUTHORIZATION'] ?? '';
 
-    final client = Client();
     final headers = {'Authorization': authorization};
 
-    final response = await client.get(Uri.parse(url), headers: headers);
+    final http.Response response;
+    try {
+      response = await _client.get(Uri.parse(url), headers: headers);
+    } catch (e) {
+      print("http request error: $e");
+      return [];
+    }
 
     if (response.statusCode != 200) {
       return [];
     }
 
-    Map<String, dynamic> map = jsonDecode(response.body);
+    Map<String, dynamic> map;
+    try {
+      map = jsonDecode(response.body);
+    } catch (e) {
+      print("json decode error: $e");
+      return [];
+    }
     final documents = List.from(map['documents']);
     List<Place> places = documents.map((doc) => Place.fromJson(doc)).toList();
 
